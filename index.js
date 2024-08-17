24791cd59d112dcb64bdb55c79a3060d79ab4fda
Jigansa <166534172+Jigansa@users.noreply.github.com>
2024-08-17 09:51:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import env from "dotenv";
 const app = express();
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import path from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const port = 3000;
